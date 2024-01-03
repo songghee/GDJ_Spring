85074153ec036c142d4e462408549ee9c0f207a0
songghee <hiya12love@naver.com>
2024-01-03 15:22:44 +0900

    0103

--- src/main/java/com/winter/app/product/ProductController.java
+++ src/main/java/com/winter/app/product/ProductController.java
@@ -1,5 +1,23 @@
 package com.winter.app.product;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/product/*")
 public class ProductController {
+
+	//list
+	@RequestMapping(value="list", method=RequestMethod.GET)
+	public void getList() {
+		
+	}
+	
+	@RequestMapping(value="detail", method=RequestMethod.GET)
+	public void getDetail(ProductDTO productDTO) {
+			
+		}
+	
 
 }
