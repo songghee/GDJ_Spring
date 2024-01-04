ff904da6a470eeffe18e28a35ce877506270c1ed
songghee <hiya12love@naver.com>
2024-01-04 12:10:23 +0900

    0104

--- src/main/java/com/winter/app/HomeController.java
+++ src/main/java/com/winter/app/HomeController.java
@@ -12,7 +12,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.winter.app.ioc.Robot;
 
 /**
  * Handles requests for the application home page.
@@ -25,10 +24,7 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	
-	
-	@Autowired
-	private Robot robot;
+
 	
 	//annotation : 설명+기능
 	@RequestMapping(value = "/", method = RequestMethod.GET)
@@ -41,8 +37,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		robot.getLeftArm().act();
-		robot.getRightArm().act();
+
 		return "index";
 	}
 	
